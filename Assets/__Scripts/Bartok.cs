067a8d78ec5c3efa1fa1f3bd2feb46c658624e3e
rpkennedy <55095545+rpkennedy@users.noreply.github.com>
2020-11-03 17:33:56 -0500

    Bartok v0.3
    Player / fanning cards

--- Assets/__Scripts/Bartok.cs
+++ Assets/__Scripts/Bartok.cs
@@ -11,11 +11,14 @@ public class Bartok : MonoBehaviour
     public TextAsset deckXML;
     public TextAsset layoutXML;
     public Vector3 layoutCenter = Vector3.zero;
+    public float handFanDegrees = 10f;
 
     [Header("Set Dynamically")]
     public Deck deck;
     public List<CardBartok> drawPile;
     public List<CardBartok> discardPile;
+    public List<Player> players;                
+    public CardBartok targetCard;
     private BartokLayout layout;
     private Transform layoutAnchor;
 
@@ -33,6 +36,7 @@ public class Bartok : MonoBehaviour
         layout = GetComponent<BartokLayout>(); 
         layout.ReadLayout(layoutXML.text);
         drawPile = UpgradeCardsList(deck.cards);
+        LayoutGame();
     }
 
     List<CardBartok> UpgradeCardsList(List<Card> lCD)
@@ -43,5 +47,71 @@ public class Bartok : MonoBehaviour
             lCB.Add(tCD as CardBartok);
         }
         return (lCB);
+    }
+
+    public void ArrangeDrawPile()
+    {
+        CardBartok tCB;
+
+        for (int i = 0; i < drawPile.Count; i++)
+        {
+            tCB = drawPile[i];
+            tCB.transform.SetParent(layoutAnchor);
+            tCB.transform.localPosition = layout.drawPile.pos;
+
+            tCB.faceUp = false;
+            tCB.SetSortingLayerName(layout.drawPile.layerName);
+            tCB.SetSortOrder(-i * 4); 
+            tCB.state = CBState.drawpile;
+        }
+    }
+
+    void LayoutGame()
+    {
+        if (layoutAnchor == null)
+        {
+            GameObject tGO = new GameObject("_LayoutAnchor");
+            layoutAnchor = tGO.transform;
+            layoutAnchor.transform.position = layoutCenter;
+        }
+        ArrangeDrawPile();
+        Player pl;
+        players = new List<Player>();
+
+        foreach (SlotDef tSD in layout.slotDefs)
+        {
+            pl = new Player();
+            pl.handSlotDef = tSD;
+            players.Add(pl);
+            pl.playerNum = tSD.player;
+        }
+        players[0].type = PlayerType.human; 
+    }
+    
+    public CardBartok Draw()
+    {
+        CardBartok cd = drawPile[0];   
+        drawPile.RemoveAt(0);          
+        return (cd);                   
+    }
+    
+    void Update()
+    {                      
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            players[0].AddCard(Draw());
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            players[1].AddCard(Draw());
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            players[2].AddCard(Draw());
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            players[3].AddCard(Draw());
+        }
     }
 }
